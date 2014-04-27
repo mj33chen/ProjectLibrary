c095cdfde790d4be5bfe506c63480c270779d2f2
Mingjian Chen <mingjian.chen@est.fib.upc.edu>
2014-04-27 16:34:32 +0200

    Update Tema.java

--- source/Ismael_Valenzuela_Garcia/Tema.java
+++ source/Ismael_Valenzuela_Garcia/Tema.java
@@ -1,71 +1,144 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-package biblioteca_1;
-
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.ArrayList; 
 
 /**
- *
+ * Cada nodo de la clasificación es un tema...
  * @author Isma
  */
 public class Tema {
+
     private String nombre;
-    private String color;
+
+    private Tema padre;
+
+    //private Tema[] hijo;
+    private ArrayList<Tema> hijo;
+
+    private int totalNodos;
+
+    //private String color;
     //No decidido
-    private ArrayList<Libro> libros;
+    //private ArrayList<Libro> libros;
 
     //-------------------------------------
     //-------------------------------------
     
-    public Tema() {
-        this.libros = new ArrayList<Libro>();
+    public Tema() 
+    {
+        this.nombre = null;
+        this.padre = null;
+        this.hijo = null;
+        this.totalNodos = 0;  
     }
     
-    public Tema(String nombre, String color) {
-        this.color = color;
+    public Tema(String nombre) {
         this.nombre = nombre;
-        this.libros = new ArrayList<Libro>();
+        this.padre = null;
+        this.hijo = null;
+        this.totalNodos = 0;
+        //this.libros = new ArrayList<Libro>();
     }
-    
-    //No decidido
-    public Tema(String nombre, String color, ArrayList<Libro> libros) {
-        this.color = color;
+
+    /**
+     *Crear un nodo con los parametros tema, nombre, y array hijo
+     *@param tema El nodo pare 
+     *@param nombre El nombre del nodo 
+     *@param child Un array de los nodos hijo
+     */ 
+    public Tema(Tema tema, String nombre, ArrayList<Tema> child)
+    {
+        this.padre = tema;
+        this.hijo = child;
+        this.totalNodos = child.size();
         this.nombre = nombre;
-        this.libros = new ArrayList<Libro>(libros);
     }
-    
+
+   
     public String getNombre() {
         return nombre;
     }
 
-    public String getColor() {
-        return color;
+    public Tema getPadre()
+    {
+        return padre;
     }
-    
-    public Libro getLibro(Integer posicion) {
-        return libros.get(posicion);
+
+    public ArrayList<Tema> getHijo()
+    {
+        return hijo;
     }
+
+    public int getNumHijo()
+    {
+        return totalNodos;
+    }
+
+    public void addHijo(Tema t) throws IOException
+    {
+        if(t != null)
+        {
+
+            hijo.add(t);
+        }
+        else 
+            throw new IOException("Nodo Invalido!");
+    }
+
+    public void deleteHijo(Tema t) throws IOException
+    {
+        if(t != null)
+        {
+            if(t.totalNodos != 0)
+                hijo.remove(t);
+            else 
+                throw new IOException("No tiene ningun nodo hijo");
+        }
+        else
+            throw new IOException("Nodo Invalido!");
+
+    }
+
+   public boolean esHermano(Tema t)
+   {
+        return (this.padre == t.padre);
+   }
     
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+     /*public String getColor() {
+        return color;
+    }*/
     
-    public void setColor(String color) {
+    /*public Libro getLibro(Integer posicion) {
+        return libros.get(posicion);
+    }*/
+
+    /*public void setColor(String color) {
         this.color = color;
-    }
+    }*/
     
     //No decidido
-    public void anadirLibro(Integer posicion, Libro libro) {
-        this.libros.add(posicion, libro);
+    /*public void anadirLibro(Libro libro) 
+    {
+	Libro aux = new Libro();
+        aux = libro;
+        libros.add(aux);
     }
     //No decidido
-    public void eliminarLibro(Integer posicion) {
-        this.libros.remove(posicion);
-    }
+    public void eliminarLibro(int id) {
+        int i = 0;
+	for(Libro l : libros)
+	{
+		if(l.getId().equal(id))
+		{
+			libros.remove(id);
+			break;
+		}
+		++i;
+	}
+    }*/
     
 }
+
