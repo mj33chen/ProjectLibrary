8ade06d9bc45fb6450f269f88f69cd1b69ee1ff2
Mingjian Chen <mingjian.chen@est.fib.upc.edu>
2014-05-02 22:57:23 +0200

    Update DriverPosicion.java

--- source/Mingjian_Chen/DriverPosicion/DriverPosicion.java
+++ source/Mingjian_Chen/DriverPosicion/DriverPosicion.java
@@ -22,6 +22,7 @@ public class DriverPosicion
 
         System.out.println("Creamos una nueva posicion" + NEW_LINE);
         Posicion p = new Posicion();
+        Libro libro = new Libro();
 
 		boolean exit = false;
 		while(! exit)
@@ -63,82 +64,101 @@ public class DriverPosicion
 
 				case 2: 
 					System.out.println("Pon un valor a la posición x:");
-			                    p.setPosX(Integer.parseInt(buffer.readLine()));
-			                    System.out.println("Parametro x se ha modificado:" + NEW_LINE);
-			
-			                    System.out.println("Pon un valor a la posició de y");
-			                    p.setPosY(Integer.parseInt(buffer.readLine()));
-			                    System.out.println("Parametro y se ha modificado:" + NEW_LINE);
-			
-			                    break;
+                    p.setPosX(Integer.parseInt(buffer.readLine()));
+                    System.out.println("Parametro x se ha modificado:" + NEW_LINE);
+
+                    System.out.println("Pon un valor a la posició de y");
+                    p.setPosY(Integer.parseInt(buffer.readLine()));
+                    System.out.println("Parametro y se ha modificado:" + NEW_LINE);
+
+                    break;
 
 				case 3:
 					System.out.println("selecciona una varible para modificar su valor" + NEW_LINE);
 					System.out.println("con la opcion 1 si desea la variable x" + NEW_LINE);
 					System.out.println("con la opcion 2 si desea la variable y" + NEW_LINE);
-		                    System.out.println("con la opcion 0 si desea salir" + NEW_LINE);
-		                    int op = Integer.parseInt(buffer.readLine());
-		                    switch (op) 
-		                    {
-		                        case 1:
-		                        	System.out.println("Pon un valor a la posición x:");
-		                            p.setPosX(Integer.parseInt(buffer.readLine()));
-		                            System.out.println("Parametro x se ha modificado:" + NEW_LINE);
-		                            break;
-		                        case 2:
-		                        	System.out.println("Pon un valor a la posició de y");
-		                            p.setPosY(Integer.parseInt(buffer.readLine()));
-		                            System.out.println("Parametro y se ha modificado:" + NEW_LINE);
-		                            break;
-		                        default:
-		                            break;
-		                    }
-		                    break;
+                    System.out.println("con la opcion 0 si desea salir" + NEW_LINE);
+                    int op = Integer.parseInt(buffer.readLine());
+                    switch (op) 
+                    {
+                        case 1:
+                        	System.out.println("Pon un valor a la posición x:");
+                            p.setPosX(Integer.parseInt(buffer.readLine()));
+                            System.out.println("Parametro x se ha modificado:" + NEW_LINE);
+                            break;
+                        case 2:
+                        	System.out.println("Pon un valor a la posició de y");
+                            p.setPosY(Integer.parseInt(buffer.readLine()));
+                            System.out.println("Parametro y se ha modificado:" + NEW_LINE);
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
 
                 case 4:
-                	System.out.println("Introduzca los datos del libro");
 
-                	System.out.println("Titulo:");
-                	String titulo = (buffer.readLine());
+                    if(p.esEmpty()) 
+                    {
+                
+                    	System.out.println("Introduzca los datos del libro");
 
-                	System.out.println("Autor:");
-                	String autor = (buffer.readLine());
+                    	System.out.println("Identificador >>");
+                        libro.setId(Integer.parseInt(buffer.readLine()));
 
-                	System.out.println("Identificador:");
-                	int id = Integer.parseInt(buffer.readLine());
+                        System.out.println("Titulo >>");
+                        libro.setTitulo(buffer.readLine());
 
-                	Libro l = new Libro(titulo, autor, id);
+                        System.out.println("Autor >>");
+                        libro.setAutor(buffer.readLine());
 
-                	System.out.println("Numero de temas: ");
-                	int num = Integer.parseInt(buffer.readLine());
+                    	System.out.println("Numero de temas: ");
+                    	int num = Integer.parseInt(buffer.readLine());
 
-                	Tema t;
-                	System.out.println("Temas asignados: ");
+                    	Tema t;
+                    	System.out.println("Introduzca nombre de tema: ");
 
-                	String line = null;
-                	int i = 0;
-		            while((line=buffer.readLine()) != null && (i < num))
-		            {
-		            	t = new Tema();
-		            	t.setNombre(line);
-				        	l.anadirTema(t);
-				        	++i;
-				        	if(i == num) System.out.println("Pon un culaquier caracter para salir: ");
-				        }
-		
-		
-		        	p.colocarLibro(l);
-		        	System.out.println("Libro colocado" + NEW_LINE);
+                    	String line = null;
+                    	int i = 0;
+                        while((line=buffer.readLine()) != null && (i < num))
+                        {
+                        	t = new Tema();
+                        	t.setNombre(line);
+    			        	libro.anadirTema(t);
+    			        	++i;
+    			        	if(i == num) System.out.println("Pon un culaquier caracter para salir: ");
+    			        }
+       
+
+                    	p.colocarLibro(libro);
+                        System.out.println("Libro colocado" + NEW_LINE);
+                    }
+                    else 
+                    {
+                        System.out.println("Posición ocupada" + NEW_LINE);
+                    }
 
                 	break;
+
+                case 5:
+                    if(! p.esEmpty()) 
+                    {
+                        p.quitarLibro();
+                        System.out.println("Libro quitado" + NEW_LINE);
+                    }
+                    else 
+                    {
+                        System.out.println("Posicion vacia, no hay libro para quitar" + NEW_LINE);
+                    }
+                    break;
 
 				case 0:
 					exit = true;
-                    			break;
+                    break;
                     
 				default: 
 					System.out.println("Opcion Invalida!");
-                	break;
+                    break;
 			}
 		}
 	}
@@ -153,11 +173,12 @@ public class DriverPosicion
 	private static void menu()
 	{
 		System.out.println("Opciones:");
-	        System.out.println("1) Consultar la informacion de posicion");
-	        System.out.println("2) Modificar los parametros de posicion");
-	        System.out.println("3) Modificar un parametro de posicion");
-	        System.out.println("4) Colocar un libro a una posicion");
-	        System.out.println("0) exit");
-	        System.out.println(">> " + NEW_LINE);
+        System.out.println("1) Consultar la informacion de posicion");
+        System.out.println("2) Modificar los parametros de posicion");
+        System.out.println("3) Modificar un parametro de posicion");
+        System.out.println("4) Colocar un libro a una posicion");
+        System.out.println("5) Quitar un libro de una posicion");
+        System.out.println("0) exit");
+        System.out.println(">> " + NEW_LINE);
 	}
 }
