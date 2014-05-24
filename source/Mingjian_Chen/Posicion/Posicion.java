eceb716065aa335b34ba3658b48ac4764556059a
Mingjian Chen <mingjian.chen@est.fib.upc.edu>
2014-05-24 19:35:46 +0200

    Update Posicion.java

--- source/Mingjian_Chen/Posicion/Posicion.java
+++ source/Mingjian_Chen/Posicion/Posicion.java
@@ -10,24 +10,38 @@ import java.io.IOException;
 public class Posicion 
 {
     
-    private int posX;
-    private int posY;
+    private double posX;
+    private double posY;
     private boolean empty;
     private Libro libro;
     
+    /**
+    * Constructor por defecto, x = 0, y = 0
+    */
     public Posicion() 
     {   
         this.empty = true;
     }
     
-    public Posicion(int posX, int posY)
+    /**
+    * Dado valor x, y, substituyen a los parametros x, y
+    * @param posX
+    * @param posY
+    */
+    public Posicion(double posX, double posY)
     {
         this.posX = posX;
         this.posY = posY;
         this.empty = true;
     }
 
-    public Posicion(int posX, int posY, Libro l) 
+    /**
+    * Dado la clase Libro, y el valor x, y, substituyen a los parametros implicito x, y
+    * @param posX
+    * @param posY
+    * @param l La clase Libro
+    */
+    public Posicion(double posX, double posY, Libro l) 
     {
         this.posX = posX;
         this.posY = posY;
@@ -35,36 +49,62 @@ public class Posicion
         this.empty = false;
     }
     
-    public int getPosX() 
+    /**
+    * Devuelve el eje x de la posicion 
+    */
+    public double getPosX() 
     {
         return posX;
     }
     
-    public int getPosY() 
+    /**
+    * Devuelve el eje y de la posicion 
+    */
+    public double getPosY() 
     {
         return posY;
     }
    
-    public void setPosX(int x) 
+    /**
+    * Dado un valor x, modifica el eje x de la posicion 
+    * @param x Valor x
+    */
+    public void setPosX(double x) 
     {
         posX = x;
     }
     
-    public void setPosY(int y) 
+    /**
+    * Dado un valor y, modifica el eje y de la posicion 
+    * @param y Valor y
+    */
+    public void setPosY(double y) 
     {
         posY = y;
     }
 
+    /**
+    * Consultar si la posicion no contiene ningun libro
+    * @return True si es vacio, false en caso contrario
+    *
+    */
     public boolean esEmpty()
     {
         return empty;
     }
 
+    /**
+    * Devuelve la clase libro de la posicion
+    * @return libro
+    */
     public Libro getLibro()
     {
         return libro;
     }
 
+    /**
+    * Quita un libro de la posicion
+    */
     public void quitarLibro()
     {
         this.empty = true;
@@ -72,9 +112,8 @@ public class Posicion
     }
 
     /**
-     *
-     * 
-     *
+     * Dado una clase libro l, coloca a la posicion
+     * @param l La clase Libro
      */
     public void colocarLibro(Libro l)
     {
