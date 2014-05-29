250435429217f480263c6747e5d7cd43e57c9edf
Mingjian Chen <mingjian.chen@est.fib.upc.edu>
2014-05-30 00:09:12 +0200

    Update ControladorBiblioteca.java

--- source/Mingjian_Chen/ControladorBiblioteca/ControladorBiblioteca.java
+++ source/Mingjian_Chen/ControladorBiblioteca/ControladorBiblioteca.java
@@ -1,3 +1,5 @@
+package CapaDominio;
+
 import java.util.ArrayList;
 import java.io.IOException;
 
@@ -11,59 +13,68 @@ public class ControladorBiblioteca
 
 	private Biblioteca biblio;
 
+	/**
+	* Constructor default
+	*/
+	public ControladorBiblioteca()
+	{
+		biblio = null;  
+	}
+
+	/**
+	* Constructor 
+	*/
 	public ControladorBiblioteca(String nombre, Clasificacion clasi)
 	{
 		this.biblio = new Biblioteca(nombre, clasi);  
 	}
 
 	/**
-	*
-	*
+	* Dado un id del libro, devuelve las informaciones(id, titulo, autor, y numero de temas del libro
+	* @param id Identificador
+	* @return list La lista de carateristicas de libro
 	*/
-	public String[] infoLibro(int id)
+	public String[] infoLibro(int id) throws IOException
 	{
-		String[] list = new String[4];
+		String[] list = new String[3];
 		Libro l = biblio.getLibro(id);
 		list[0] = Integer.toString(l.getId());
 		list[1] = l.getTitulo();
         list[2] = l.getAutor();
-        list[3] = Integer.toString(l.getNumTemas());
         return list;
 	}
 
 	/**
-	*
-	*
+	* Devuelve las informaciones de libros que contiene
+	* @return list Lista de libros
 	*/
 	public ArrayList<String[]> getCatalogo() 
 	{
 		ArrayList<String[]> list = new ArrayList<String[]>();
-		for (int i = 0; i < biblio.librosSize(); ++i) 
+		for (int i = 0; i < biblio.biblioSize(); ++i) 
 		{
-			String[] v = new String[4];
+			String[] v = new String[3];
 			Libro l = biblio.getLibroIndex(i);
 			v[0] = Integer.toString(l.getId());
 			v[1] = l.getTitulo();
 	        v[2] = l.getAutor();
-	        v[3] = Integer.toString(l.getNumTemas());
 	        list.add(v);
 	    }
 	    return list;   
 	}
 
 	/**
-	*
-	*
+	* Generar la matriz de flujo 
 	*
 	*/
-	public double[][] GenerarMatriz()
+	public double[][] generarMatriz() throws IOException
 	{
 		return biblio.fluxMatrix();
 	}
 
 	/**
-	*
-	*
+	* Consultar el nombre de la biblioteca
+	* @return biblio.getNombre() El nombre de la biblioteca 
 	*/
 	public String consultarNombreBiblio()
 	{
@@ -71,16 +82,17 @@ public class ControladorBiblioteca
 	}
 
 	/**
-	*
-	*
+	* Modificar el nombre de la bibliteca
+	* @param nombre 
 	*/
 	public void modificarNombreBiblio(String nombre) 
 	{
 		biblio.setNombre(nombre);
 	}
+
 	/**
-	*
-	*
+	* Consultar el nombre de la clasificacion asignada
+	* @return biblio.getClasificacion().getNombre() Nombre de la clasificacion
 	*/
 	public String consultarNombreClasificacion()
 	{
@@ -88,8 +100,8 @@ public class ControladorBiblioteca
 	}
 
 	/**
-	*
-	*
+	* Reasignar la clasificacion a la biblioteca
+	* @param c La clase Clasificacion
 	*/
 	public void setClasificacion(Clasificacion c)
 	{
@@ -98,19 +110,23 @@ public class ControladorBiblioteca
 
 
 	/**
-	*
-	*
+	* Dado un id, un titulo, y nombre de autor, modifica las carateristicas del libro
+	* @param id identificador
+	* @param titulo Nombre del titulo
+	* @param autor Nombre del autor
 	*/
-	public void modificarLibro(int id, String titulo, String autor) 
+	public void modificarLibro(int id, String titulo, String autor) throws IOException
 	{
 		biblio.modificarLibro(id, titulo, autor);
 	}
 
 	/**
-	* PRIMERO CREAR EL LIBRO, Y LUEGO ASIGNAR LOS TEMAS AL LIBRO !!!!!!·$%"·$·%·"
-	*
+	* Dado un id, un titulo, y nombre de autor, añade un libro con estas carateristicas 
+	* @param id identificador
+	* @param titulo Nombre del titulo
+	* @param autor Nombre del autor
 	*/
-	public void anadirLibro(int id, String titulo, String autor)
+	public void anadirLibro(int id, String titulo, String autor) throws IOException
 	{
 		Libro l = new Libro();
 		l.setId(id);
@@ -120,41 +136,61 @@ public class ControladorBiblioteca
 	}
 
 	/**
-	*
-	*
-	*/
-	public void anadirTemaLibro(String nombre, String tema, int nivel, Tema padre) throws IOException
+    * Dado un nombre de titulo, un nombre tema añade el tema con el nombre de la variable nombre_ al libro
+    * @param id Identificador del libro
+    * @param nombre_ Nombre de tema
+    */
+	public void anadirTemaLibro(int id, String tema) throws IOException
 	{
-		//Tema t = new Tema();
-		//t.setNombre(tema);
-		biblio.anadirTemaLibro(nombre, tema, nivel, padre);
+		biblio.anadirTemaLibro(id, tema);
 	}
 
 	/**
-	*
-	*
-	*/
-	public void eliminarLibro(int id) 
+    * Dado un identificador del libro, lo elimina de la Biblioteca
+    * @param id Identificador del libro
+    */
+	public void eliminarLibro(int id) throws IOException
 	{
 		biblio.eliminarLibro(id);
 	}
 
 	/**
-	*
-	*
-	*/
+    * Consulta si la Biblioteca es vacía, es decir, si no contiene ningun libro
+    * @return True Si no contiene ningun libro
+    *          False en caso contrario
+    */
 	public boolean isEmpty()
 	{
 		return biblio.biblioEmpty();
 	}
 
 	/**
+    *
+    *
+    *
+    */
+    public boolean esLibroValido(int id)
+    {
+    	return biblio.esLibroValido(id);
+    }
+
+	/**
+    * Devuelve el numero total de libros en la Biblioteca
+    * @return libros.size() El numero total de libros
+    *
+    */
+	public int size()
+	{
+		return biblio.biblioSize();
+	}
+
+	/**
 	*
 	*
 	*/
-	public int size()
+	public Biblioteca getBiblioteca()
 	{
-		return biblio.librosSize();
+		return biblio;
 	}
 
 }
