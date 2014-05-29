7885b2ee31b5cf5c43baaeee0c186ee75fe6c924
Mingjian Chen <mingjian.chen@est.fib.upc.edu>
2014-05-30 00:05:41 +0200

    input,output del controlador no está actualizado

--- source/Mingjian_Chen/DriverClasificacion/DriverClasificacion.java
+++ source/Mingjian_Chen/DriverClasificacion/DriverClasificacion.java
@@ -20,8 +20,8 @@ public class DriverClasificacion
 		InputStreamReader iostream = new InputStreamReader(System.in);
         BufferedReader buffer = new BufferedReader (iostream);
 
-        System.out.println("Creamos una nueva clasificacion");
-        System.out.println("Pon un nombre de la clasificacion" + NEW_LINE);
+        System.out.println("Creamos una nueva clasificacion" + NEW_LINE);
+        System.out.println("Pon un nombre de la clasificacion >> ");
         String nombre = (buffer.readLine());
         String color;
         Clasificacion clasifi = new Clasificacion(nombre);
@@ -55,14 +55,7 @@ public class DriverClasificacion
                     nombre = buffer.readLine();
                     tema = clasifi.getTema(nombre);
 
-                    System.out.println("Nivel: " + tema.getNivel());
-                    System.out.println("Numº de hijos: " + tema.getNumHijo());
-                    System.out.println(tema.toString());
-
-                    if(tema.getNombre().equals("General"))
-                        System.out.println("Tema General");
-                    else 
-                        System.out.println("Nombre de tema padre: " + tema.getPadre().getNombre());
+                    System.out.println("Nombre " + nombre);
                     System.out.println("Color: " + tema.getColor() + NEW_LINE);
                 	break;
 
@@ -77,7 +70,7 @@ public class DriverClasificacion
                     System.out.println("Introduzca color >> ");
                     tema.setColor(buffer.readLine());
 
-                    tema_padre = clasifi.getTema(nombre_padre);
+                    tema_padre = new Tema(nombre_padre);
                     clasifi.anadirTema(tema, tema_padre);
                     System.out.println("Tema añadido!"  + NEW_LINE);
                     break;
@@ -85,7 +78,7 @@ public class DriverClasificacion
                 case 6:
                     System.out.println("Introduzca un nombre del tema para eliminar desde la clasificacion >> ");
                     nombre = buffer.readLine();
-                    clasifi.desvincularHijos(nombre);
+                    clasifi.eliminarTema(nombre);
                     System.out.println("Tema(s) eliminado(s)!"  + NEW_LINE);
                     break;
 
@@ -108,13 +101,41 @@ public class DriverClasificacion
                         System.out.println("color >> ");
                         color = buffer.readLine();
 
-                        System.out.println("nombre del tema padre >> ");
-                        nombre_padre = buffer.readLine();
-
-                        clasifi.modificarTema(nombre, nombre_nuevo, color, nombre_padre);
+                        clasifi.modificarTema(nombre, nombre_nuevo, color);
                     }
 
                     System.out.println("Tema modificado!"  + NEW_LINE);
+                    break;
+
+                case 8:
+                    System.out.println("Introducid el nombre del tema >> ");
+                    nombre = buffer.readLine();
+
+                    System.out.println("Introducid el nombre del tema >> ");
+                    String nombre2 = buffer.readLine();
+
+                    double simi = clasifi.similitud(nombre, nombre2);
+                    System.out.println("La similaridad entre tema " + nombre + " y tema " + nombre2 + " es " + simi);
+                    break;
+
+                case 9:
+                    System.out.println("Introducid el nombre del tema >> ");
+                    nombre = buffer.readLine();
+
+                    System.out.println("Introducid el nombre del tema >> ");
+                    nombre2 = buffer.readLine();
+
+                    String p = clasifi.padreComun(nombre, nombre2);
+                    System.out.println("El padre comun entre tema " + nombre + " y tema " + nombre2 + " es " + p);
+                    break;
+
+                case 10:
+                    System.out.println("Introducid un nombre del tema >> ");
+                    nombre  = (buffer.readLine());
+                    if(clasifi.esTemaValido(nombre)) 
+                        System.out.println("Tema es valido.");
+                    else 
+                        System.out.println("Tema no es valido.");
                     break;
 
 				case 0:
@@ -145,6 +166,9 @@ public class DriverClasificacion
         System.out.println("5) Añadir tema");
         System.out.println("6) Eliminar tema");
         System.out.println("7) Modificar tema");
+        System.out.println("8) Calcular la similaridad entre dos temas");
+        System.out.println("9) Padre comun de dos temas");
+        System.out.println("10) Consultar si el tema es valido");
         System.out.println("0) exit");
         System.out.println(">> " + NEW_LINE);
 	}
