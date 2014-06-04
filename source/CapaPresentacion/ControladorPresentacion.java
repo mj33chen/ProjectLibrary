1b46e1d0febe58dab36aa1e40ee98d2469c475d9
Mingjian Chen <mingjian.chen@est.fib.upc.edu>
2014-06-04 13:10:37 +0200

    Update ControladorPresentacion.java

--- source/CapaPresentacion/ControladorPresentacion.java
+++ source/CapaPresentacion/ControladorPresentacion.java
@@ -2,69 +2,112 @@ package CapaPresentacion;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import CapaDominio.ControladorBiblioteca;
-import CapaDominio.ControladorClasificacion;
+
+import CapaDominio.ControladorSistema;
 
 /**
  * Created by Mingjian on 29/05/2014.
  */
 public class ControladorPresentacion
 {
-    private ControladorClasificacion controlClasi;
-    private ControladorBiblioteca controlBiblio;
+
+    private ControladorSistema controlSystem;
+    private VistaPrincipal vistaPrin = null;
 
     public ControladorPresentacion()
     {
-        this.controlClasi = new ControladorClasificacion();
-        this.controlBiblio = new ControladorBiblioteca();
+       vistaPrin = new VistaPrincipal(this);
+       controlSystem = new ControladorSistema();
+    }
+
+    public void initPresentation()
+    {
+        vistaPrin.setVisible(true);
+    }
+
+    public void rebootController()
+    {
+        controlSystem.rebootController();
+    }
+
+    public void activeView(String dir, String item)
+    {
+        vistaPrin.activeView(dir, item);
     }
 
     public void anadirLibro(String titulo, String autor, int id) throws IOException
     {
-        controlBiblio.anadirLibro(id, titulo, autor);
+        controlSystem.anadirLibro(titulo, autor, id);
+    }
+
+    public void modificarLibro(String titulo, String autor, int id) throws IOException
+    {
+        controlSystem.modificarLibro(titulo, autor, id);
     }
 
     public void eliminarLibro(String idLibro) throws IOException
     {
-        int id = Integer.parseInt(idLibro);
-        controlBiblio.eliminarLibro(id);
+        controlSystem.eliminarLibro(idLibro);
     }
 
     public boolean esEmptyBiblio()
     {
-        return controlBiblio.isEmpty();
+        return controlSystem.esEmptyBiblio();
+    }
+
+    public boolean esEmptyClasifi()
+    {
+        return controlSystem.esEmptyClasifi();
     }
 
     public ArrayList<String> getAllThemes()
     {
-        return controlClasi.getNombreTemas();
+        return controlSystem.getAllThemes();
     }
 
     public ArrayList<String> getTemas(int id) throws IOException
     {
-        return controlBiblio.getTemas(id);
+        return controlSystem.getTemas(id);
+    }
+
+    public String getPadre(String nombre) throws IOException
+    {
+        return controlSystem.getPadre(nombre);
+    }
+
+    public void eliminarTema(String nombre) throws IOException
+    {
+        controlSystem.eliminarTema(nombre);
+    }
+
+    public boolean esTemaValido(String nombre) throws IOException
+    {
+        return (controlSystem.esTemaValido(nombre));
+    }
+
+    public void modificarTema(String nombre, String nombre_nuevo) throws IOException
+    {
+        controlSystem.modificarTema(nombre, nombre_nuevo);
     }
 
     public ArrayList<String[]> getAllLibros()
     {
-        return controlBiblio.getCatalogo();
+        return controlSystem.getAllLibros();
     }
 
     public String[] getLibro(String idLibro) throws IOException
     {
-        int id = Integer.parseInt(idLibro);
-        return controlBiblio.infoLibro(id);
+        return controlSystem.getLibro(idLibro);
     }
 
     public void asignarTemaLibro(String idL, String nombreT) throws IOException
     {
-        int id = Integer.parseInt(idL);
-        controlBiblio.anadirTemaLibro(id, nombreT);
+        controlSystem.asignarTemaLibro(idL, nombreT);
     }
 
     public Boolean temaRepetido(String idL, String nombreT) throws IOException
     {
-        int id = Integer.parseInt(idL);
-        return controlBiblio.esTemaRepetido(id, nombreT);
+        return controlSystem.temaRepetido(idL, nombreT);
     }
+
 }
